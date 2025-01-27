7702d52b977d2894e5c979336a450c79e8e15156
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-01-28 08:52:44 +0900

    add file

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ import torch
 import pytorch_lightning as pl
 from lightning.pytorch.loggers import TensorBoardLogger
 import torch.utils.data as data
-from solver import LitGAN
+from solver import LitDAE
 import torch.utils.data as dat
 import torch.multiprocessing as mp
 from speech_dataset import SpeechDataset
@@ -19,10 +19,12 @@ warnings.filterwarnings('ignore')
 def main(args, config:dict):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
 
-    mean, var = C.load_mean_var(config['stat_path'])
-    mean = rearrange(mean, ' b (f c) -> b f c', c=1).to(device)
-    var = rearrange(var, ' b (f c) -> b f c', c=1).to(device)
-    model = LitGAN.load_from_checkpoint(args.checkpoint,
+    input_mean, input_var, output_mean, output_var = C.load_mean_var(config['stat_path'])
+    input_mean = rearrange(input_mean, ' b (f c) -> b f c', c=1).to(device)
+    input_var = rearrange(input_var, ' b (f c) -> b f c', c=1).to(device)
+    output_mean = rearrange(output_mean, ' b (f c) -> b f c', c=1).to(device)
+    output_var = rearrange(output_var, ' b (f c) -> b f c', c=1).to(device)
+    model = LitDAE.load_from_checkpoint(args.checkpoint,
                                         strict=False,
                                         config=config).to(device)
     model.eval()
@@ -30,13 +32,13 @@ def main(args, config:dict):
     df = pd.read_csv(args.csv)
     for index, row in df.iterrows():
         mel = torch.load(df['noisy']).to(device)
-        mel = (mel - mean)/var
+        mel = (mel - input_mean)/input_var
         original_length = mel.shape[-1]
         split_mel = S.split_and_reshape(mel, config['num_frames'])
         mask = torch.ones_like(split_mel, device=split_mel.device)
         output = model.forward(split_mel, mask)
         output = S.reshape_back(output, original_length)
-        output = output * var + mean
+        output = output * output_var + output_mean
         
 if __name__ == '__main__':
     torch.multiprocessing.set_start_method('spawn', force=True)
